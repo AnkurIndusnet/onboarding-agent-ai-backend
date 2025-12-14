1bb1880d35c51f6738175210b15920da7477cc6c
Ankur_Ghosh <ankur.ghosh@intglobal.com>
2025-12-15 00:48:47 +0530

    firstCommit

---
+++ src/main/java/com/example/onboardingAgent/onboardingAgent/OnboardingAgentApplication.java
@@ -0,0 +1,13 @@
+package com.example.onboardingAgent.onboardingAgent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnboardingAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnboardingAgentApplication.class, args);
+	}
+
+}
